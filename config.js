cf15ddf74135fccb1044f605a048ddfa15d00544
aJakeoo <82735127+aJakeoo@users.noreply.github.com>
2026-08-24 10:38:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyZ1uXioJsvF-_hMDT0GceIAECHWYFWHrU6qN_JanvqqhtCE1S0G4FL0RBj31Ua1rYp/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx46SWMyrbtbOd-5lxB7ZlI8o-kZy3oLFMUBKr73fNzKRPna70GRUr2flJCYCoWB5H9/exec",
   RESULTS_PASSWORD: "jake",
   MAX_FIVES: 5
 };
